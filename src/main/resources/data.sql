3df21a3e124337308ef70aec909535b741c2a8d9
MatthieuBlm <matthieu.bellamy@isabo.ai>
2020-01-03 10:51:50 +0100

    Add Matthieu's login

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
 INSERT INTO user (username, password) VALUES ('Lucas', '$2a$10$J0OTjmOJYRPSNJFyc3IzrONaLzUkPw6Qwo.QojOlr0y4s7zHEhl3G');
+INSERT INTO user (username, password) VALUES ('Matthieu', '$2y$10$g42L2YCevF6ZsEzIMZmluufR0ga2GVOrmNEe5hoEIqOCxzuHazxvO');
